510a64176aa54d595519ec2b6f10f0cf347c59f9
Jamie Hirvonen <141663898+jamiehirvo03@users.noreply.github.com>
2023-11-28 11:50:01 +1100

    no message

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,4 +15,32 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    //Player spawns in 
+        //Reset values to default
+        //Prepare game as a new level
+        //Debug Starting values
+
+    //Player Death (triggered by touching spikes)
+        //Play death sound
+        //Display particle system (blood)
+        //Debug death confirmation
+
+    //Background music system
+        //Randomly play music from a selection, if a track has already been played it will not be chosen again
+        //Debug when track ends, changes and what track is playing
+
+    //Coin Collection when player touches it
+        //Add the coin to players coin count
+        //Update coin count on UI
+        //Remove the coin object from scene so it cannot be collected again
+        //Play subtle particle effect
+        //Play sound effect
+        //Debug coin count
+
+    //Interactable signs if player is close enough 
+        //Check if player is within trigger range
+        //Check for prompted input
+        //Display text on UI
+        //Debug what message was displayed
 }
